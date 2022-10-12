65cf79b48b780cce38886d16907bd9eb462a1cca
KT_Hyun <106125968+Amami-satoko@users.noreply.github.com>
2022-10-12 17:13:52 +0900

    2022-10-12

--- 03.CSS_학습/04.3DAni/js/cube.js
+++ 03.CSS_학습/04.3DAni/js/cube.js
@@ -1,40 +1,56 @@
-// 큐브 애니 js - cube.js
+// 큐브애니 JS - cube.js
 
-window.addEventListener("DOMContentLoaded", function(){
-    console.log("로딩 완료");
+/////////////// 로드구역 //////////////////////
+window.addEventListener("DOMContentLoaded",
+function(){
 
-    // 클릭 이벤트 대상 : .btns button
+    // 호출확인
+    console.log("로딩완료!");
+
+    // 클릭이벤트 대상: .btns button
     const btns = document.querySelector(".btns button");
 
-    // 변경 대상 : .cube
-    const cube = document. querySelector(".cube");
+    // 변경대상: .cube
+    const cube = document.querySelector(".cube");
 
-    // 클릭 시 기능 구현 함수
+    // 클릭시 기능구현함수
     const aniFn = function(){
-        console.log("aniFn 실행");
 
-        // 대상 선정 : .cube -> cube변수
+        // 1. 호출확인
+        console.log("나야나!");
 
-        // 변경 내용 : 변경대상요소에 클래스 넣기/빼기
+        // 2. 대상선정: .cube -> cube변수
+
+        // 3. 변경내용: 변경대상요소에 클래스 넣기/빼기
         cube.classList.toggle("on");
 
-        // 큐브 클래스 on여부에 따라 버튼 글자 변경
-        cube.classList.contains("on") ? btns.innerText="멈춰!" : btns.innerText="돌아!"
-        console.log(cube.classList.contains("on"));
+        // 4. 큐브 클래스 on여부에 따라 버튼 글자 변경하기
+        // 비?집:놀이동산 -> 삼항연산자
+        cube.classList.contains("on") ? 
+        btns.innerText="멈춰!" : btns.innerText="돌아!";
+        // console.log(cube.classList.contains("on"));
 
-        /*
+        /*********************************** 
             [ 클래스 컨트롤 객체 ]
             classList
             -> 하위 메서드
-            1. add(클래스명) - 클래스 추가
-            2. remove(클래스명) - 클래스 제거
-            3. toggle(클래스명) - 클래스 유무에 따른 추가/제거
-            4. contains(클래스명) - 지정한 클래스가 있으면 true, 없으면 false
-        */ 
-    }
+            1. add(클래스명) 
+                -> 클래스추가
+            2. remove(클래스명) 
+                -> 클래스제거
+            3. toggle(클래스명) 
+                -> 클래스 유무에 따라 추가/제거
+            4. contains(클래스명)
+                -> 지정한 클래스가 있으면 true
+                (없으면 false)
+        ***********************************/
 
-    // 대상에 클릭 설정하기
+    }; //////// aniFn함수 ////////////////
+
+    // 대상에 클릭설정하기
     btns.onclick = aniFn;
-    // 전달값이 없다면 소괄호 생략한 함수명만 이벤트 속성에
-    // 할당하면 바로 실행되지 않고 이벤트가 발생할 때 실행된다
-});
+    // 전달값이 없다면 소괄호 생략한 함수명만 이벤트속성에
+    // 할당하면 바로 실행되지 않고 이벤트가 발생할때 실행된다!
+
+
+}); /////////////// 로드구역 ///////////////////
